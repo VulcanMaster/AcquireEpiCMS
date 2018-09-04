c7c2b6046fa02025b4e7a9b83313af4c86c64c47
Dmitrijs Blinkovs <dmitry.blinkov@outlook.com>
2018-09-04 14:56:11 +0300

    Created the page type "StandardPage" in "\Models\Pages"

---
+++ AcquireEpiCMS.Web/Models/Pages/StandardPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AcquireEpiCMS.Web.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "37eed0d1-f489-49a4-bd71-dec7df92c3eb", Description = "")]
+    public class StandardPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "The page heading.",
+            GroupName = SystemTabNames.Content,
+            Order = 0)]
+        public virtual XhtmlString Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
